4b385d3050914b271f9604ed338efede567f9d4b
Jason Olimpio <olimpiojason@gmail.com>
2025-11-10 07:22:40 +0100

    Update Next.js configuration for GitHub Pages deployment with a fixed base path and remove outdated deployment instructions from README.md.

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,11 @@ import type { NextConfig } from 'next'
 const withNextIntl = createNextIntlPlugin('./src/app/i18n/request.ts')
 
 const isGithubPages = process.env.GITHUB_PAGES === 'true'
-const basePath = isGithubPages ? process.env.NEXT_PUBLIC_BASE_PATH || '' : ''
 
 const nextConfig: NextConfig = {
   ...(isGithubPages && {
     output: 'export',
-    basePath: basePath,
+    basePath: '/store-hub',
     images: {
       unoptimized: true
     },
